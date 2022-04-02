63300d891b7623ebc6e84f0c07336fd75ce51730
mukundtheamateur <mukundshukla000@gmail.com>
2022-04-02 22:13:45 +0530

    newcommit

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -23,7 +23,7 @@ const SearchBox = ({ history }) => {
         className='mr-sm-2 ml-sm-5'
       ></Form.Control>
 
-      <Button type='submit' variant='outline-success' style="margin-top:10px;">
+      <Button type='submit' variant='outline-success'>
         Search
       </Button>
     </Form>
